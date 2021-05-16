9a6aa025a1da607dbe88621c8066eed6354c6ad1
DannRainer <dann.rainer@gmail.com>
2021-05-16 11:16:05 +0300

    cops logic, color change, cinemachine

---
+++ MetroRushHour/Assets/Scripts/Dann/ColorChange.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    Color endColor;
+    Color playerColor;
+    void Start()
+    {
+        //playerColor = GetComponent<MeshRenderer>().material.color;
+        endColor = new Color(255f, 0f, 0f, 100f);
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.C))
+        {
+            GetComponent<MeshRenderer>().material.color = Color.Lerp(GetComponent<MeshRenderer>().material.color, endColor, Time.deltaTime / 2);
+        }
+    }
+}
